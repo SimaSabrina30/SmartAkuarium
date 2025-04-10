f416bd9d658ab87ae6102063c6b3f25c9446cc1c
Annisa Maulida Zuhriah <annisamaulidazuhriah23@mhs.politala.ac.id>
2025-04-10 15:53:56 +0800

    update login

--- app/src/main/java/com/sima/smartakuarium/MainActivity.java
+++ app/src/main/java/com/sima/smartakuarium/MainActivity.java
@@ -1,24 +1,42 @@
 package com.sima.smartakuarium;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etUsername, etPassword;
+    Button btnSignIn;
+    TextView tvForgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        tvForgotPassword = findViewById(R.id.tvForgotPassword);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = etUsername.getText().toString().trim();
+                String pass = etPassword.getText().toString().trim();
+
+                if (user.equals("admin") && pass.equals("admin123")) {
+                    Toast.makeText(MainActivity.this, "Login Berhasil!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Username atau Password salah", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        tvForgotPassword.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "Fitur lupa password belum tersedia", Toast.LENGTH_SHORT).show();
         });
     }
 }
