9e74672a21c6d1a8de75a305c67a066ad27e32bd
Rahmawati <rahmawati15122004@gmail.com>
2025-04-15 15:37:40 +0800

    detailakuarium

---
+++ app/src/main/java/com/sima/smartakuarium/detailakuarium.java
@@ -0,0 +1,21 @@
+package com.sima.smartakuarium;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class detailakuarium extends AppCompatActivity {
+    Button btnRefresh;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnRefresh = findViewById(R.id.btnRefresh);
+        btnRefresh.setOnClickListener(view -> {
+            Toast.makeText(this, "Data diperbarui!", Toast.LENGTH_SHORT).show();
+        });
+    }
+}
