ab54bc1ee8bf7f689e6c7c5f06fe14ef4e64e22e
SimaSabrina30 <simasabrina23@mhs.politala.ac.id>
2025-05-06 13:14:19 +0800

    update profile

--- app/src/main/java/com/sima/smartakuarium/ProfileActivity.java
+++ app/src/main/java/com/sima/smartakuarium/ProfileActivity.java
@@ -1,10 +1,15 @@
 package com.sima.smartakuarium;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.core.graphics.Insets;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -13,11 +18,37 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        // Pastikan ID "main" ada di layout activity_profile.xml
+        // Mengatur padding sesuai sistem bar
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Tombol back
+        ImageButton btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Kembali ke Beranda
+                Intent intent = new Intent(ProfileActivity.this, beranda.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        // Tombol Logout
+        Button btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Arahkan ke MainActivity dan hapus backstack
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish(); // Tutup ProfileActivity
+            }
         });
     }
 }
